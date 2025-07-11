6b16346714000441689cc7915e5f055c5a8efa55
Alex Mata <alexmatasoftware@gmail.com>
2025-07-11 14:56:15 -0500

    following lessons

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
+
+// Loading assets (images, music, animations, etc.)
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+}
+
+function create() {
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  this.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+}
 
 const config = {
+  // WebGL
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  parent: 'app',
   physics: {
+    // Arcade physics plugin, manages physics simulation
     default: 'arcade',
     arcade: {
       gravity: { y: 200 }
     }
   },
-  parent: 'app',
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create
   }
 };
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
